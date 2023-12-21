9af7355d50287883acace0bd1663f0ba52728051
aucejito <tomanotademicorreo@hotmail.com>
2023-12-22 00:32:42 +0100

    Add PEP8 compliant

--- auth.py
+++ auth.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python3
 
 import sys
+import webbrowser
+
 import spotipy
 import tidalapi
-import webbrowser
 import yaml
 
+
 def open_spotify_session(config):
-    credentials_manager = spotipy.SpotifyOAuth(username=config['username'],
-				       scope='playlist-read-private',
-				       client_id=config['client_id'],
-				       client_secret=config['client_secret'],
-				       redirect_uri=config['redirect_uri'])
+    credentials_manager = spotipy.SpotifyOAuth(
+        username=config["username"],
+        scope="playlist-read-private",
+        client_id=config["client_id"],
+        client_secret=config["client_secret"],
+        redirect_uri=config["redirect_uri"],
+    )
     try:
         credentials_manager.get_access_token(as_dict=False)
     except spotipy.SpotifyOauthError:
-        sys.exit("Error opening Spotify sesion; could not get token for username: ".format(config['username']))
+        sys.exit(
+            "Error opening Spotify sesion; could not get token for username:"
+            + config["username"]
+        )
 
     return spotipy.Spotify(oauth_manager=credentials_manager)
 
-def open_tidal_session(config = None):
+
+def open_tidal_session(config=None):
     try:
         with open('.session.yml', 'r') as session_file:
             previous_session = yaml.safe_load(session_file)
@@ -32,9 +40,11 @@ def open_tidal_session(config = None):
         session = tidalapi.Session()
     if previous_session:
         try:
-            if session.load_oauth_session(token_type= previous_session['token_type'],
-                                   access_token=previous_session['access_token'],
-                                   refresh_token=previous_session['refresh_token'] ):
+            if session.load_oauth_session(
+                token_type=previous_session["token_type"],
+                access_token=previous_session["access_token"],
+                refresh_token=previous_session["refresh_token"],
+            ):
                 return session
         except Exception as e:
             print("Error loading previous Tidal Session: \n" + str(e) )
@@ -47,10 +57,13 @@ def open_tidal_session(config = None):
     webbrowser.open(url)
     future.result()
     with open('.session.yml', 'w') as f:
-        yaml.dump( {'session_id': session.session_id,
-                   'token_type': session.token_type,
-                   'access_token': session.access_token,
-                   'refresh_token': session.refresh_token}, f )
+        yaml.dump(
+            {
+                "session_id": session.session_id,
+                "token_type": session.token_type,
+                "access_token": session.access_token,
+                "refresh_token": session.refresh_token,
+            },
+            f,
+        )
     return session
-
-
--- tidalapi_patch.py
+++ tidalapi_patch.py
@@ -1,26 +1,39 @@
 from tqdm import tqdm
 
+
 def _remove_indices_from_playlist(playlist, indices):
     headers = {'If-None-Match': playlist._etag}
     index_string = ",".join(map(str, indices))
-    playlist.requests.request('DELETE', (playlist._base_url + '/items/%s') % (playlist.id, index_string), headers=headers)
+    playlist.requests.request(
+        "DELETE",
+        (playlist._base_url + "/items/%s") % (playlist.id, index_string),
+        headers=headers,
+    )
     playlist._reparse()
 
+
 def clear_tidal_playlist(playlist, chunk_size=20):
-    with tqdm(desc="Erasing existing tracks from Tidal playlist", total=playlist.num_tracks) as progress:
+    with tqdm(
+        desc="Erasing existing tracks from Tidal playlist",
+        total=playlist.num_tracks,
+    ) as progress:
         while playlist.num_tracks:
             indices = range(min(playlist.num_tracks, chunk_size))
             _remove_indices_from_playlist(playlist, indices)
             progress.update(len(indices))
-    
+
+
 def add_multiple_tracks_to_playlist(playlist, track_ids, chunk_size=20):
     offset = 0
-    with tqdm(desc="Adding new tracks to Tidal playlist", total=len(track_ids)) as progress:
+    with tqdm(
+        desc="Adding new tracks to Tidal playlist", total=len(track_ids)
+    ) as progress:
         while offset < len(track_ids):
             count = min(chunk_size, len(track_ids) - offset)
-            playlist.add(track_ids[offset:offset+chunk_size])
+            playlist.add(track_ids[offset : offset + chunk_size])
             offset += count
             progress.update(count)
+
 
 def set_tidal_playlist(playlist, track_ids):
     clear_tidal_playlist(playlist)
