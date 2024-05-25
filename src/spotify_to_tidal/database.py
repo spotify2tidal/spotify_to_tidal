d9a183806bd61d43c0097ad67d1dfee0e32554b7
joshrmcdaniel <80354972+joshrmcdaniel@users.noreply.github.com>
2024-05-25 09:47:34 -0500

    type

--- src/spotify_to_tidal/database.py
+++ src/spotify_to_tidal/database.py
@@ -1,10 +1,13 @@
 import datetime
+import os
+
+from typing import Union
 import sqlalchemy
 from sqlalchemy import Table, Column, String, DateTime, MetaData, insert, select, update, delete
 
 
 class Database:
-    def __init__(self, filename='.cache.db'):
+    def __init__(self, filename: os.PathLike='.cache.db'):
         self.engine = sqlalchemy.create_engine(f"sqlite:///{filename}")
         meta = MetaData()
         self.match_failures = Table('match_failures', meta,
@@ -15,7 +18,7 @@ class Database:
                                     sqlite_autoincrement=False)
         meta.create_all(self.engine)
 
-    def _get_next_retry_time(self, insert_time=None):
+    def _get_next_retry_time(self, insert_time: Union[datetime.datetime, datetime.timedelta]=None) -> datetime.datetime:
         if insert_time:
             # double interval on each retry
             interval = 2 * (datetime.datetime.now() - insert_time)
@@ -23,10 +26,11 @@ class Database:
             interval = datetime.timedelta(days=7)
         return datetime.datetime.now() + interval
 
-    def cache_match_failure(self, track_id):
+    def cache_match_failure(self, track_id: int) -> None:
         """ notifies that matching failed for the given track_id """
         fetch_statement = select(self.match_failures).where(
             self.match_failures.c.track_id == track_id)
+        connection: sqlalchemy.engine.Connection
         with self.engine.connect() as connection:
             with connection.begin():
                 # Either update the next_retry time if track_id already exists, otherwise create a new entry
@@ -40,7 +44,7 @@ class Database:
                     connection.execute(insert(self.match_failures), {
                                        "track_id": track_id, "insert_time": datetime.datetime.now(), "next_retry": self._get_next_retry_time()})
 
-    def has_match_failure(self, track_id):
+    def has_match_failure(self, track_id: int) -> bool:
         """ checks if there was a recent search for which matching failed with the given track_id """
         statement = select(self.match_failures.c.next_retry).where(
             self.match_failures.c.track_id == track_id)
@@ -50,10 +54,11 @@ class Database:
                 return match_failure.next_retry > datetime.datetime.now()
             return False
 
-    def remove_match_failure(self, track_id):
+    def remove_match_failure(self, track_id: int) -> None:
         """ removes match failure from the database """
         statement = delete(self.match_failures).where(
             self.match_failures.c.track_id == track_id)
+        connection: sqlalchemy.engine.Connection
         with self.engine.connect() as connection:
             with connection.begin():
                 connection.execute(statement)
--- src/spotify_to_tidal/search.py
+++ src/spotify_to_tidal/search.py
@@ -1,15 +1,12 @@
 import tidalapi
 import tidalapi.session
 
-from cachetools import TTLCache, cached
-
 def simple(input_string: str) -> str:
     # only take the first part of a string before any hyphens or brackets to account for different versions
     return input_string.split('-')[0].strip().split('(')[0].strip().split('[')[0].strip()
 
 # We simplify the search to easily cache results
 
-@cached(TTLCache(maxsize=1024, ttl=600))
 def _search_tidal(
     tidal_session: tidalapi.Session,
     q: str,
@@ -17,13 +14,11 @@ def _search_tidal(
     ) -> tidalapi.session.SearchResults:
     return tidal_session.search(q, models=[model])
 
-@cached(TTLCache(maxsize=1024, ttl=600))
 def search_tidal_albums(tidal_session: tidalapi.Session, artist: str, album: str):
     artist = simple(artist.casefold())
     album = simple(album.casefold())
     return _search_tidal(tidal_session, artist+" "+album, tidalapi.Album)
 
-@cached(TTLCache(maxsize=1024, ttl=600))
 def search_tidal_tracks(tidal_session: tidalapi.Session, artist: str, track: str):
     artist = simple(artist.casefold())
     track = simple(track.casefold())
